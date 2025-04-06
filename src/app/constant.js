246344d5d18c1125a734376453f81096cfd9d9d3
notripovercrip <notripovercrip@gmail.com>
2025-04-06 11:09:50 +0530

    changed to backup  render server

--- src/app/constant.js
+++ src/app/constant.js
@@ -1,2 +1,3 @@
 // export const BACKEND_URL = "https://api.revivefest.in"
-export const BACKEND_URL = "https://reviveserver.onrender.com"
+// export const BACKEND_URL = "https://reviveserver.onrender.com"
+export const BACKEND_URL = "https://reviveserver-puns.onrender.com"
